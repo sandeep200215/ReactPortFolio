74a138f6db143154f869138ab3d6b8014b48e56d
sandeep200215 <sandeepkumar24y7@gmail.com>
2024-06-05 22:20:15 +0530

    some changes in footer file

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -5,47 +5,72 @@ import mypicture from './assets/mypicture.jpg'
 function Contact()
 {
 return (
-    <footer className="bg-gray-800 align-bottom sm:py-12">
-        <h2 className='text-white text-xl text-center font-bold'>Contact Me</h2>
-        <h3></h3>
-      <div className="container   mx-auto flex justify-evenly px-4 font-semibold">
-        {/* Left part of the footer */}
-        <div className="text-white  ">
-       
-         <ul>
-            <li>
-            <a href="https://www.linkedin.com/in/sandeepkr1506/" target='_blank' className='text-white-500 hover:underline'>LinkedIn</a>
-            </li>
-            <li>
-            <a href="https://www.hackerrank.com/profile/coder1235"  target='_blank' className='text-white-500 hover:underline'>HackerRank</a>
-            </li>
-            <li>
-            <a href="https://leetcode.com/u/sandeep1506/" target='_blank' className='text-white-500 hover:underline'>LeetCode</a>
-            </li>
-        
-         </ul>
-      
-        </div>
-        
-        {/* Right part of the footer */}
-        <div className="text-white">
+  <footer className="bg-gray-800  pb-24">
+       <h2 class="text-center text-white pb-4 text-xl">Contact Me</h2>
+      <div className="container mx-auto flex justify-center items-center ">
+        <div className="flex space-x-6 text-3xl">
+          <a href="https://www.linkedin.com/in/sandeepkr1506/" className="text-gray-300 hover:text-white">
+          <i class="fa-brands fa-linkedin"></i>
+              
+          </a>
+          <a href="https://www.hackerrank.com/profile/coder1235" className="text-gray-300 hover:text-white">
+          <i class="fa-brands fa-hackerrank"></i>
            
-         <ul>
-           
-            <li>
-            <a href="https://www.instagram.com/sandeep_sharma8670/" target='_blank' className='text-white-500 hover:underline'>Instagram</a>
-            </li>
-            <li>
-            <a href="https://telegram.me/sandeep_34ty" target='_blank' className='text-white-500 hover:underline'>Telegram</a>
-            </li>
-            <li>
-            <a href="https://www.facebook.com/profile.php?id=100054210267040" target='_blank' className='text-white-500 hover:underline'>Facebook</a>
-            </li>
-         </ul>
-      
+          </a>
+          <a href="https://www.instagram.com/sandeep_sharma8670/" className="text-gray-300 hover:text-white">
+          <i class="fa-brands fa-instagram"></i>
+          </a>
+          <a href="https://telegram.me/sandeep_34ty" className="text-gray-300 hover:text-white">
+          <i class="fa-brands fa-telegram"></i>
+          </a>
+          <a href="https://www.facebook.com/profile.php?id=100054210267040" className="text-gray-300 hover:text-white">
+          <i class="fa-brands fa-facebook"></i>
+          </a>
         </div>
       </div>
     </footer>
-)
-}
+//     <footer className="bg-gray-800 align-bottom sm:py-12">
+//         <h2 className='text-white text-xl text-center font-bold'>Contact Me</h2>
+//         <h3></h3>
+//       <div className="   mx-auto    ">
+//         {/* Left part of the footer */}
+//         <div className="  text-white  ">
+       
+//          <ul className='text-4xl flex '>
+//             <li className='mx-10'>
+//             <a href="https://www.linkedin.com/in/sandeepkr1506/" target='_blank' className='  text-white-500 hover:text-blue-50'><i class="fa-brands fa-linkedin"></i></a>
+//             </li>
+//             <li>
+//             <a href="https://www.hackerrank.com/profile/coder1235"  target='_blank' className='mx-10 text-white-500 hover:underline'><i class="fa-brands fa-hackerrank"></i></a>
+//             </li>
+//             <li>
+//             <a href="https://leetcode.com/u/sandeep1506/" target='_blank' className=' mx-10 text-white-500 hover:underline'><i class="cib-leetcode"></i></a>
+//             </li>
+//             <li>
+//             <a href="https://www.instagram.com/sandeep_sharma8670/" target='_blank' className='mx-10 text-white-500 hover:underline'><i class="fa-brands fa-instagram"></i></a>
+//             </li>
+//             <li>
+//             <a href="https://telegram.me/sandeep_34ty" target='_blank' className='mx-10 text-white-500 hover:underline'><i class="fa-brands fa-telegram"></i></a>
+//             </li>
+//             <li>
+//             <a href="https://www.facebook.com/profile.php?id=100054210267040" target='_blank' className='mx-10 text-white-500 hover:underline'><i class="fa-brands fa-facebook"></i></a>
+//             </li>
+        
+//          </ul>
+      
+//         </div>
+        
+//         {/* Right part of the footer */}
+//         <div className="text-white">
+           
+//          <ul>
+           
+            
+//          </ul>
+      
+//         </div>
+//       </div>
+//     </footer>
+// )
+)}
 export default Contact;
