4ef8cf2ad9c1f40354240e81c8a8b931c510bf07
sandeep200215 <sandeepkumar24y7@gmail.com>
2024-06-07 11:07:49 +0530

    Again change

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -42,7 +42,7 @@ function Navbar() {
           <div className="mr-2 flex md:hidden ">
             <button
               onClick={toggleNavbar}
-              className="inline-flex items-center justify-center p-2 rounded-md mx-2 text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+              className="inline-flex items-center justify-center p-2 rounded-md mx-3 text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
             >
               <svg
                 className="h-10 w-10"
