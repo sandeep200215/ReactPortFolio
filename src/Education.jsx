9caf127f28ff9461096bc74eb408f7dc0f1e3240
sandeep200215 <sandeepkumar24y7@gmail.com>
2024-06-10 12:49:20 +0530

    add icon of education

--- src/Education.jsx
+++ src/Education.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 
 const Education = () => {
   return (
-    <section className=" bg-cover bg-center h-screen py-8 sm:py-16 bg-gray-100 mt-8">
+    <section className=" bg-cover bg-center h-screen py-8 sm:py-16 bg-gray-100 mt-0">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl sm:text-2xl font-bold text-center mb-8 ">Education Details</h2>
+        <div>
+          <span>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNTGiMUuTg8ba4bkK9m90b59xqxTZW_jRUgA&usqp=CAU" className='h-16 w-16 mx-auto rounded-lg '/> </span>
+        <div className="text-3xl sm:text-2xl font-bold text-center mb-8 ">Education Details</div>
+
+        </div>
+       
         <div className="overflow-x-auto">
           <table className="  w-full table-auto mb-12">
             <thead className=''>
