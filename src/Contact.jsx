7c26a941152aec595ce81d1b345052d20ca1646b
sandeep200215 <sandeepkumar24y7@gmail.com>
2024-06-22 15:00:27 +0530

    change

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -10,11 +10,11 @@ return (
        <h2 class="text-center  text-white pb-auto pt-auto text-xl  font-mono font-semibold ">Contact Me</h2>
       <div className="container mx-auto flex justify-center items-center  ">
         <div className="flex space-x-6 text-xl mt-4">
-          <a href="https://www.linkedin.com/in/sandeepkr1506/" className="bg-gradient-to-r from-[#0077B5] to-[#005E94] rounded-full p-1 px-2 hover:text-blue-300">
+          <a href="https://www.linkedin.com/in/sandeepkr1506/" target='_blank' className="bg-gradient-to-r from-[#0077B5] to-[#005E94] rounded-full p-1 px-2 hover:text-blue-300">
           <i class="fa-brands fa-linkedin"></i>
               
           </a>
-          <a href="https://www.hackerrank.com/profile/coder1235" className="bg-gradient-to-r from-[#2EC866] to-[#007A3E] p-1 rounded-full px-2 hover:text-yellow-300">
+          <a href="https://www.hackerrank.com/profile/coder1235" target='_blank' className="bg-gradient-to-r from-[#2EC866] to-[#007A3E] p-1 rounded-full px-2 hover:text-yellow-300">
           <i class="fa-brands fa-hackerrank"></i>
            
           </a>
@@ -31,21 +31,21 @@ return (
         </div>
       </div>
       <span className='flex my-6  justify-center items-center text-xl space-x-6'>
-          <a href="https://telegram.me/sandeep_34ty" className="bg-gradient-to-r from-blue-500 to-blue-700 rounded-full  p-1 px-2 hover:text-blue-300">
+          <a href="https://telegram.me/sandeep_34ty" target='_blank' className="bg-gradient-to-r from-blue-500 to-blue-700 rounded-full  p-1 px-2 hover:text-blue-300">
           <i class="fa-brands fa-telegram"></i>
         
           </a>
-          <a href="https://www.facebook.com/profile.php?id=100054210267040" className="bg-gradient-to-r from-blue-600 to-indigo-700 rounded-full p-1  px-2 hover:text-blue-300">
+          <a href="https://www.facebook.com/profile.php?id=100054210267040" target='_blank' className="bg-gradient-to-r from-blue-600 to-indigo-700 rounded-full p-1  px-2 hover:text-blue-300">
           <i class="fa-brands fa-facebook"></i>
           </a>
-          <a href="https://www.instagram.com/sandeep_sharma8670/" className="bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 rounded-full p-1 px-2 hover:text-red-300">
+          <a href="https://www.instagram.com/sandeep_sharma8670/" target='_blank' className="bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500 rounded-full p-1 px-2 hover:text-red-300">
           <i class="fa-brands fa-instagram"></i>
           </a>
           
           </span>
           <div className='flex   justify-center items-center space-x-4 '>
             <p className='text-white '>Drop Mail Me :- </p>
-          <a href="mailto:sandeepkumar24y7@gmail.com" className="bg-gradient-to-r from-blue-500 to-blue-700 rounded-full p-1 px-2 hover:text-gray-400">
+          <a href="mailto:sandeepkumar24y7@gmail.com" target='_blank' className="bg-gradient-to-r from-blue-500 to-blue-700 rounded-full p-1 px-2 hover:text-gray-400">
           <i class="fa-solid fa-envelope"></i>
           </a>
           </div>
